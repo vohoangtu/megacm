81d07c9596126397c4b7ba02d036c745768e9b03
hoangtu <vohoangtu0409@gmail.com>
2023-09-21 02:47:11 +0700

    #2: MegaCM

--- views/base.blade.php
+++ views/base.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel</title>
+    <title>MegaCM</title>
     @vite([
     'resources/css/bootstrap.scss',
     'resources/js/app.js'
